e6bb1b805e68f85cf7a74df00c6062a1363d3938
Vladpers1k <vladik340123@gmail.com>
2024-12-01 17:40:01 +0200

    123

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-const moviesListElement = document.getElementById('movies-list')
-const searchInput = document.getElementById('search')
+const movieListElement = document.getElementById('movies-list')
+const searchInput = document.querySelector('.search__input')
 const searchCheckbox = document.getElementById('checkbox')
 
 let isSearchTriggerEnabled = false
 let lastSearchQuery = null
 
-const debounceTime = (() => {
+const debounceTime = (cb, ms) => {
   let timer = null
-  return (cb, ms) => {
-    if (timer) {
-      clearTimeout(timer)
-      timer = null
-    }
-    timer = setTimeout(cb, ms)
+  return (...args) => {
+    if (timer) clearTimeout(timer)
+    timer = setTimeout(() => cb(...args), ms)
   }
-})()
+}
 
 const getData = (url) =>
   fetch(url)
-    .then((res) => res.json())
+    .then((res) => {
+      if (!res.ok) throw new Error(`HTTP error! Status: ${res.status}`)
+      return res.json()
+    })
     .then((data) => {
-      if (!data || !data.Search) throw 'The server returned incorrect data'
+      if (!data || !data.Search) throw new Error('Incorrect data')
       return data.Search
     })
 
@@ -30,32 +30,38 @@ const addMovieToList = ({ Poster: poster, Title: title, Year: year }) => {
 
   item.classList.add('movie')
   img.classList.add('movie__image')
-  img.src = /^(https?:\/\/)/i.test(poster) ? poster : 'images/nothing.jpg'
-  img.alt = `${title} ${year}`
-  img.title = `${title} ${year}`
+
+  img.src = poster && /^(https?:\/\/)/i.test(poster) ? poster : 'images/nothing.jpg'
+  img.alt = `${title} (${year})`
+  img.title = `${title} (${year})`
 
   item.append(img)
-  moviesListElement.prepend(item)
+  movieListElement.prepend(item)
 }
 
 const clearMoviesMarkup = () => {
-  if (moviesListElement) moviesListElement.innerHTML = ''
+  if (movieListElement) movieListElement.innerHTML = ''
 }
 
-const inputSearchHandler = (e) => {
-  debounceTime(() => {
-    const searchQuery = e.target.value.trim()
+const inputSearchHandler = debounceTime((e) => {
+  const searchQuery = e.target.value.trim()
 
-    if (!searchQuery || searchQuery.length < 4 || searchQuery === lastSearchQuery) return
-    if (!isSearchTriggerEnabled) clearMoviesMarkup()
+  if (!searchQuery || searchQuery.length < 4 || searchQuery === lastSearchQuery) return
 
-    getData(`https://www.omdbapi.com/?apikey=18b8609f&s=${searchQuery}`)
-      .then((movies) => movies.forEach((movie) => addMovieToList(movie)))
-      .catch((err) => console.error(err))
+  if (!isSearchTriggerEnabled) clearMoviesMarkup()
 
-    lastSearchQuery = searchQuery
-  }, 1000)
-}
+  getData(`https://www.omdbapi.com/?apikey=18b8609f&s=${searchQuery}`)
+    .then((movies) => {
+      if (!movies || movies.length === 0) {
+        console.log('Фільми не знайдено')
+        return
+      }
+      movies.forEach((movie) => addMovieToList(movie))
+    })
+    .catch((err) => console.error(err))
+
+  lastSearchQuery = searchQuery
+}, 1500)
 
 searchInput.addEventListener('input', inputSearchHandler)
 searchCheckbox.addEventListener('change', (e) => (isSearchTriggerEnabled = e.target.checked))
